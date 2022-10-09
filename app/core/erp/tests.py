35578658792e5e9553fd87a0e7665208d0483af4
juansilva <juandsr@hotmail.com>
2022-10-09 18:37:17 -0400

    datatables installed

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,13 +1,2 @@
-from django.test import TestCase
-from config.wsgi import *
-from core.erp.models import *
 
-data = ['Milk and dairy products', 'Meat, fish and eggs', 'Potatoes, legumes, nuts',
-         'Vegetables', 'Fruits', 'Cereals and derivatives, sugar and sweets',
-         'Fats, oil and butter']
-
-for i in data:
-    cat = Category(name=i)
-    cat.save()
-    print('Saving Register N°{}'.format(cat.id))
 
